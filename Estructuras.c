f7606e522162b13882750618114f0993e9ad95da
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2024-01-16 20:43:19 -0500

    Primer commit estructura y variable para menu

---
+++ Estructuras.c
@@ -0,0 +1,18 @@
+//Definimos las librerias para el programa
+#include <stdio.h>
+#include <string.h>
+//Creamos la estructura 
+struct alumno
+{
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota;
+};
+//Creamos la funcion main junto con una variable para crear un menu
+int main()
+{
+    int opcion;
+    struct alumno alumnoActual;
+}
